56836e7e81d2aa19599a062cb5a6af21ca2ec8ac
Juan Carlos <gurquiza8@gmail.com>
2024-06-02 16:31:04 -0600

    Creating category DTO

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,6 +3,7 @@
 
 export * from './dtos/auth/login-user.dto';
 export * from './dtos/auth/register-user.dto';
+export * from './dtos/category/create-category.dto';
 
 export * from './entities/user.entity';
 
--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,5 +1,5 @@
 import { Response, Request } from 'express';
-import { CustomError } from '../../domain';
+import { CreateCategoryDto, CustomError } from '../../domain';
 
 export class CategoryController {
 
@@ -15,8 +15,10 @@ export class CategoryController {
     }
 
     createCategory = async(req: Request, res: Response) => {
+        const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
+        if ( error ) return res.status(400).json({ error });
 
-        res.json('Create Category')
+        res.json(createCategoryDto);
     }
 
     getCategories = async(req: Request, res: Response) => {
