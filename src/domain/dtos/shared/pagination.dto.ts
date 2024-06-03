9c5b5466dd1b97682b60197acbbd91e1143d98d7
Juan Carlos <gurquiza8@gmail.com>
2024-06-02 19:12:35 -0600

    Apply pagination

--- src/domain/dtos/shared/pagination.dto.ts
+++ src/domain/dtos/shared/pagination.dto.ts
@@ -4,7 +4,7 @@ export class PaginationDto {
 
     private constructor(
         public readonly page: number,
-        public readonly limmit: number,
+        public readonly limit: number,
     ) {}
 
     static create( page: number = 1, limit: number = 10 ): [string?, PaginationDto?] {
--- src/presentation/services/category.service.ts
+++ src/presentation/services/category.service.ts
@@ -1,5 +1,5 @@
 import { CategoryModel } from '../../data';
-import { CreateCategoryDto, CustomError, UserEntity } from '../../domain';
+import { CreateCategoryDto, CustomError, PaginationDto, UserEntity } from '../../domain';
 
 export class CategoryService {
 
@@ -32,19 +32,36 @@ export class CategoryService {
 
     }
 
-    async getCategories() {
+    async getCategories( paginationDto: PaginationDto ) {
+
+        const { page, limit } = paginationDto;
 
         try {
 
-            const categories = await CategoryModel.find();
-            if ( !categories ) throw CustomError.badRequest('Not exists categories');
+            // const total = await CategoryModel.countDocuments();
+            // const categories = await CategoryModel.find()
+            //     .skip( (page - 1) * limit )
+            //     .limit( limit );
+            const [total, categories] = await Promise.all([
+                CategoryModel.countDocuments(),
+                CategoryModel.find()
+                    .skip( (page - 1) * limit )
+                    .limit( limit )
+            ]);
 
-            return categories.map( category => ({
-                id: category.id,
-                name: category.name,
-                available: category.available,
-            }));
-            
+            return {
+                page: page,
+                limit: limit,
+                total: total,
+                next: `/api/categories?page=${(page - 1)}&limit=${limit}`,
+                prev: (page - 1 > 0) ? `/api/categories?page=${(page - 1)}&limit=${limit}` : null,
+                categories: categories.map( category => ({
+                    id: category.id,
+                    name: category.name,
+                    available: category.available,
+                }))
+            } 
+                
         } catch (error) {
             console.log(error);
             throw CustomError.internalServer('Internal Server Error');
