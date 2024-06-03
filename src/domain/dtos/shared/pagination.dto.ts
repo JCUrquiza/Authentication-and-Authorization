c5541466b39b4f7aa8505cff03e142ba24444a03
Juan Carlos <gurquiza8@gmail.com>
2024-06-02 18:57:46 -0600

    Creating paginationDTO

---
+++ src/domain/dtos/shared/pagination.dto.ts
@@ -0,0 +1,21 @@
+
+
+export class PaginationDto {
+
+    private constructor(
+        public readonly page: number,
+        public readonly limmit: number,
+    ) {}
+
+    static create( page: number = 1, limit: number = 10 ): [string?, PaginationDto?] {
+
+        if ( isNaN(page) || isNaN(limit) ) return ['Page and limit must be numbers', undefined];
+        if ( page <= 0 ) return ['Page must be greather than 0'];
+        if ( limit <= 0 ) return ['Limit must be greather than 0'];
+
+        return [undefined, new PaginationDto(page, limit)];
+    }
+
+}
+
+
