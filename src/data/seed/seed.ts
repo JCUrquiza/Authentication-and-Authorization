48ac3e3dc83fcdf36d13affb8032cf6771c2f52f
Juan Carlos <gurquiza8@gmail.com>
2024-06-03 14:53:47 -0600

    Function to fill database with seed

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,6 @@
 import { envs } from '../../config';
-import { MongoDatabase } from '../mongo/mongo-database';
+import { CategoryModel, MongoDatabase, ProductModel, UserModel } from '../mongo';
+import { seedData } from './data';
 
 (async() => {
 
@@ -13,7 +14,44 @@ import { MongoDatabase } from '../mongo/mongo-database';
     await MongoDatabase.disconnect();
 })();
 
+const randomBetween0AndX = ( x: number ) => {
+    return Math.floor( Math.random() * x );
+}
+
 async function main() {
+
+    // Borrar la base
+    await Promise.all([
+        UserModel.deleteMany(),
+        CategoryModel.deleteMany(),
+        ProductModel.deleteMany(),
+    ]);
+
+    // 1- Crear Usuarios
+    const users = await UserModel.insertMany( seedData.users );
+
+    // 2.- Crear Categorías
+    const categories = await CategoryModel.insertMany(
+        seedData.categories.map( category => {
+            return {
+                ...category,
+                user: users[0]._id
+            }
+        })
+    );
+    
+    // 3.- Crear Productos
+    const products = await ProductModel.insertMany(
+        seedData.products.map( product => {
+            return {
+                ...product,
+                user: users[randomBetween0AndX( seedData.users.length - 1 )]._id,
+                category: categories[randomBetween0AndX( seedData.categories.length - 1 )]._id,
+            }
+        })
+    );
+
+    console.log('SEEDED');
     
 }
 
